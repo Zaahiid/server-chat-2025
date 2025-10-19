fc590c0fd70475da5c24828efb213c099f5394f2
Zaahiid <zahidrsheikh01@gmail.com>
2025-10-19 21:59:26 +0530

    Implement MongoDB connection and update server initialization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { connectDB } from "./lib/db";
+
 
 
 const app = express();
 
-app.listen(3000, () => {console.log(`Server is runnig on port 3000`)})
+app.listen(3000, () => {
+    console.log(`Server is runnig on port 3000`);
+    connectDB();
+})
