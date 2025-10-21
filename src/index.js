ab460e543dfbcbe8cd9605b7dbe1772f6712568b
Zaahiid <zahidrsheikh01@gmail.com>
2025-10-21 07:52:23 +0530

    Add authentication routes and implement checkAuth controller

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js"
 import { app, server } from "./lib/socket.js"
 
 
 dotenv.config();
 
 const  PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
 
 server.listen(PORT, () => {
     console.log(`Server is runnig on port ${PORT}`);
