78e0000b1cfbf6e5c4036fedbeafa60e870e4a27
Zaahiid <zahidrsheikh01@gmail.com>
2025-10-22 07:52:34 +0530

    Add message routing and controller for chat functionality

---
+++ src/controllers/message.controller.js
@@ -0,0 +1,62 @@
+import { getRecieverSocketId } from "../lib/socket.js";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggdInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggdInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error  in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+    });
+
+    await newMessage.save();
+
+    const receiverSocketId = getRecieverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js"
+import messagesRoutes from "./routes/message.route.js"
 import { app, server } from "./lib/socket.js"
 
 
@@ -11,6 +12,7 @@ dotenv.config();
 const  PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messagesRoutes)
 
 server.listen(PORT, () => {
     console.log(`Server is runnig on port ${PORT}`);
